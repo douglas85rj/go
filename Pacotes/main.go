ec2dd1a809f8b7d481928abf2cb1b720d38e8cdb
douglas85rj <douglas85rj@gmail.com>
2024-12-30 15:38:51 -0300

    comentários

--- Pacotes/main.go
+++ Pacotes/main.go
@@ -9,3 +9,10 @@ func main() {
 	fmt.Println("Escrevendo do arquivo main")
 	auxiliar.Escrever()
 }
+
+//se uma função começa com letra maiúscula, ela é pública e pode ser acessada por outros pacotes
+//se uma função começa com letra minúscula, ela é privada e só pode ser acessada dentro do mesmo pacote
+//para executar o código, é necessário rodar o comando go run main.go
+//para compilar o código, é necessário rodar o comando go build main.go
+//para executar o código compilado, é necessário rodar o comando ./main
+//para instalar o pacote, é necessário rodar o comando go install
